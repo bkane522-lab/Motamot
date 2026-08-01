8a81d89895d8d8bc89d7336592adf5782b511366
bkane522-lab <bkane522@gmail.com>
2026-08-01 22:15:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'motamot-v2';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'motamot-v4';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/dictation.js'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
